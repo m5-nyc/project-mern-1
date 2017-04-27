ce7b5701bd7c0e64acbe75b3fa469092c1ee39af
Christopher Maverick <cmaverickce@gmail.com>
2017-04-27 09:05:10 -0400

    login forms

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,3 +1,65 @@
-/**
- * Created by M5 on 2017-04-27.
- */
+import React, { Component } from'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router';
+import { loginUser } from '../../actions';
+
+const form = reduxForm({
+    form: 'login',
+});
+
+
+class Login extends Component {
+    handleFormSubmit(formProps) {
+        this.props.loginUser(formProps);
+    }
+
+    renderAlert() {
+        if (this.props.errorMessage) {
+            return (
+                <div>
+                    <span><strong>ERROR!</strong> {this.props.errorMessage}</span>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        const {handleSubmit} = this.props;
+        return (
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                {this.renderAlert()}
+                <div className="row">
+                    <div className="col-md-6">
+                        <label>Email</label>
+                        <Field name="email"
+                               className="form-control"
+                               component={renderField}
+                               type="text" />
+                    </div>
+                    <div className="col-md-6">
+                        <label>Password</label>
+                        <Field name="password"
+                               className="form-control"
+                               component={renderField}
+                               type="password" />
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-primary">
+                        Login
+                    </button>
+                </div>
+            </form>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        errorMessage: state.auth.error,
+        message: state.auth.message
+    };
+}
+
+export default connect(mapStateToProps, { loginUser })(form(Login));
--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,3 +1,102 @@
-/**
- * Created by M5 on 2017-04-27.
- */
+import React, { Component } from'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+import { registerUser} from '../../actions';
+
+const form = reduxForm({
+    form: 'register',
+    validate
+});
+
+const renderField = field => (
+    <div>
+        <input className="form-control" {...field.input} />
+        {field.touched && field.error && <div className="error">{field.error}</div>div>}
+    </div>
+);
+
+function validate(formProps){
+    const errors = {};
+
+    if(!formProps.firstName){
+        error.firstName = 'Please enter a first name';
+    }
+
+    if(!formProps.lastName){
+        errors.lastName = 'Please enter a last name';
+    }
+
+    if(!formProps.email){
+        errors.email = 'Please enter an email';
+    }
+
+    if(!formProps.password){
+        errors.password = 'Please enter a password';
+    }
+
+    return errors;
+}
+
+class Register extends Component {
+    handleFormSubmit(formProps) {
+        this.props.registerUser(formProps);
+    }
+
+    renderAlert() {
+        if (this.props.errorMessage) {
+            return (
+                <div>
+                    <span><strong>ERROR!</strong> {this.props.errorMessage}</span>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        const {handleSubmit} = this.props;
+        return (
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                {this.renderAlert()}
+                <div className="row">
+                    <div className="col-md-6">
+                        <label>First Name</label>
+                        <Field name="firstName"
+                               className="form-control"
+                               component={renderField}
+                               type="text" />
+                    </div>
+                    <div className="col-md-6">
+                        <label>Last Name</label>
+                        <Field name="lastName"
+                               className="form-control"
+                               component={renderField}
+                               type="text" />
+                    </div>
+                    <div className="col-md-6">
+                        <label>Email</label>
+                        <Field name="email"
+                               className="form-control"
+                               component={renderField}
+                               type="text" />
+                    </div>
+                    <div className="col-md-6">
+                        <label>Password</label>
+                        <Field name="password"
+                               className="form-control"
+                               component={renderField}
+                               type="password" />
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        errorMessage: state.auth.error,
+        message: state.auth.message
+    };
+}
+
+export default connect(mapStateToProps, { registerUser })(form(Register));
