ff49b6e1fb15f4ce43cde56a044e089a8f5d54be
Christopher Maverick <cmaverickce@gmail.com>
2017-04-27 08:37:41 -0400

    action creators template done

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,6 @@
-/**
- * Created by M5 on 2017-04-27.
- */
+export const AUTH_USER = 'auth_user',
+             UNAUTH_USER = 'unauth_user',
+             AUTH_ERROR = 'auth_error',
+             FORGOT_PASSWORD_REQUEST = 'forgot_password_request',
+             RESET_PASSWORD_REQUEST = 'reset_password_request',
+             PROTECTED_TEST = 'protected_test';
